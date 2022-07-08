ca201446a8f964fce93e5adc2bc8b88911ea4d72
Ivan <ivan.kranjec95@gmail.com>
2022-07-08 20:40:46 +0200

    [Genre entity] - add initial version of Genre entity.

---
+++ Bookstore/Models/Genre.cs
@@ -0,0 +1,13 @@
+﻿namespace Bookstore.Models
+{
+    public class Genre
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<Book> Books { get; set; }
+
+        public DateTime? CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+        public DateTime? DeletedAt { get; set; }
+    }
+}
