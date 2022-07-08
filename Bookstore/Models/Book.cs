e41ffd3459f60d4adf000aa8c87143f9e9c8f79d
Ivan <ivan.kranjec95@gmail.com>
2022-07-08 20:40:25 +0200

    [Book entity] - add initial version of Book entity.

---
+++ Bookstore/Models/Book.cs
@@ -0,0 +1,19 @@
+﻿namespace Bookstore.Models
+{
+    public class Book
+    {
+        public Guid Id { get; set; }
+        public Author Author { get; set; }
+        public Guid AuthorId { get; set; }
+        public string Title { get; set; }
+        public Genre Genre { get; set; }
+        public Guid GenreId { get; set; }
+        public double Price { get; set; }
+        public DateTime PublishedAt { get; set; }
+        public string Description { get; set;  }
+
+        public DateTime? CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+        public DateTime? DeletedAt { get; set; }
+    }
+}
