bfe5c0d34f73f3e999d57a82cfc098c81814b543
Ivan <ivan.kranjec95@gmail.com>
2022-07-08 21:34:55 +0200

    [BorrowedBook entity] - add new entity.

---
+++ Bookstore/Models/BorrowedBook.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bookstore.Models
+{
+    [Index(nameof(IdentityUserId), nameof(BookId), nameof(BorrowedAt))]
+    public class BorrowedBook
+    {
+        public Guid Id { get; set; }
+        public IdentityUser IdentityUser { get; set; }
+        public string IdentityUserId { get; set; }
+        public Book Book { get; set; }
+        public Guid BookId { get; set; }
+        public DateTime? BorrowedAt { get; set; }
+        public DateTime? ReturnedAt { get; set; }
+
+        public DateTime? CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+        public DateTime? DeletedAt { get; set; }
+    }
+}
