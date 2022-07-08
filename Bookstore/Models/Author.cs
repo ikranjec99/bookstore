e04671a49de7bfe1de5db7a61a9ead2a2aaf0240
Ivan <ivan.kranjec95@gmail.com>
2022-07-08 20:41:06 +0200

    [Author entity] - add initial version of Author entity.

---
+++ Bookstore/Models/Author.cs
@@ -0,0 +1,13 @@
+﻿namespace Bookstore.Models
+{
+    public class Author
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public ICollection<Book> Books { get; set; }
+        public DateTime? CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+        public DateTime? DeletedAt { get; set; }
+    }
+}
